821dab3782024aa02a5d7e627114e2ff58814fda
Itskleii <84471738+Itskleii@users.noreply.github.com>
2021-06-07 20:35:23 +0200

    Update ready.js

--- Events/ready.js
+++ Events/ready.js
@@ -2,7 +2,7 @@ module.exports = async(client) => {
     client.user.setPresence({
     activity: {
       name: "dev by klei - prefix: *",
-      type: "STREAMING",
+      type: "COMPETING",
       url: "https://www.twitch.tv/itsklei_", 
     },
     status: "dnd"
