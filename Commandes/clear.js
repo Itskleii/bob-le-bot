4219004294121561bf014d4880c31f2e6619d912
Itskleii <84471738+Itskleii@users.noreply.github.com>
2021-06-10 10:29:23 +0200

    Update clear.js

--- Commandes/clear.js
+++ Commandes/clear.js
@@ -30,12 +30,7 @@ module.exports.run = async (client, message, args) => {
                     embeded.setDescription(`${args[0]} Message(s) ont été supprimé(s)`)
                     embeded.setColor('GREEN')
                     embeded.setFooter(`Par : ${message.author.tag}`)
-                    message.channel.send(embeded) 
-                    message.delete({ timeout: 3000 })
-
-                
-                }
-
+                    message.channel.send(embeded).then(m => m.delete({ timeout: 3000 })).catch(console.error); } 
 
 module.exports.help = {
     name: 'clear'
