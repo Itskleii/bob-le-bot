215aa73adf1c77b902978be3b9dea6c884cadfe6
Itskleii <84471738+Itskleii@users.noreply.github.com>
2021-07-01 10:59:46 +0200

    Update ban.js

--- Commandes/ban.js
+++ Commandes/ban.js
@@ -3,12 +3,37 @@ const Discord = require('discord.js');
 module.exports.run = (client, message, args) => {
     message.delete()
 
-    if (!message.guild.member(message.author).hasPermission('BAN_MEMBERS')) { return message.channel.send("Vous n'avez pas les permissions de bannir un membre **[BAN_MEMBERS]**"); }
-    if (!message.guild.member(client.user).hasPermission('BAN_MEMBERS')) { return message.channel.send("Je n'ai pas les permission de bannir ce membre **(Je suis hiérarchiquement inférieur)**"); }
-    if (message.mentions.users.size === 0) { return message.channel.send("Vous n'avez pas mentionner le membre que vous souhaitez bannir. **(@discord#0001)**"); }
+    var embednoperm = new Discord.MessageEmbed()
+    .setColor('RED') 
+    .setTitle('ERREUR')
+    .setDescription(`:x: Vous n'avez pas la permission de bannir un utilisateur, **[BAN_MEMBERS]**`)
+    .setTimestamp()
+
+    var embednopermbot = new Discord.MessageEmbed() 
+    .setColor('RED') 
+    .setTitle("ERREUR")
+    .setDescription(`:x: Je n'ai pas la permission de bannir cet utilisateur`)
+    .setTimestamp()
+
+    var nomention = new Discord.MessageEmbed()
+    .setColor('RED')
+    .setTitle("ERREUR")
+    .setDescription(`:x: Vous n'avez pas mentionner le membre que vous souhaitez bannir, **(@discord#0001)**`)
+    .setTimestamp()
+
+
+    var nofound = new Discord.MessageEmbed()
+    .setColor('RED')
+    .setTitle("ERREUR")
+    .setDescription(`La personne que vous souhaitez bannir est introuvable`)
+
+
+    if (!message.guild.member(message.author).hasPermission('BAN_MEMBERS')) { return message.channel.send(embednoperm); }
+    if (!message.guild.member(client.user).hasPermission('BAN_MEMBERS')) { return message.channel.send(embednopermbot); }
+    if (message.mentions.users.size === 0) { return message.channel.send(nomention); }
 
         let banMember = message.guild.member(message.mentions.users.first());
-        if (!banMember) { return message.channel.send("La personne que vous souhaitez bannir est introuvable."); }
+        if (!banMember) { return message.channel.send(nofound); }
 
         let reason = args.slice(1).join(" ");
 
