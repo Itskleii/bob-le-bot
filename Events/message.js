b98f7d94942d991113da01bdcd582abf5c089dab
Itskleii <84471738+Itskleii@users.noreply.github.com>
2021-08-16 12:36:26 +0200

    Update message.js

--- Events/message.js
+++ Events/message.js
@@ -1,15 +1,13 @@
 const  Discord = require("discord.js");
 const prefix = "*";
-const { get } = require('http');
- module.exports = async(client, message) => {
-  if(message.channel.type === "dm") return;
-    
-    if(!message.content.startsWith(prefix)) return;
-    const args = message.content.slice(prefix.length).trim().split(/ +/g);
-    const commande = args.shift();
-    const cmd = client.commands.get(commande);
-    if(!cmd) return;
-    cmd.run(client, message, args);
- }
 
- 
+ module.exports = async(client, message) => {
+  if (message.channel.type === "dm") return;
+    
+  if (!message.content.startsWith(prefix) || message.author.bot) return;
+  const args = message.content.slice(prefix.length).trim().split(/ +/g);
+  const commande = args.shift().toLowerCase();
+  const cmd = client.commands.get(commande) || client.commands.find(cmd => cmd.help.aliases && cmd.help.aliases.includes(commande));
+  if (!cmd) return;
+  cmd.run(client, message, args);
+ } 
